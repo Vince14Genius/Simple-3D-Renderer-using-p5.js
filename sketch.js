96a1bfddf7223e9df2b838a7aa34741bc620dad3
Vince14Genius <vince14genius@icloud.com>
2018-12-05 10:52:12 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,35 +1,10 @@
-// Variables used for fps
-var frameTimes = [] // Time recordings corresponding to each frame
-let oldTime = 0 // Time at which the previous frame is updated
-let fpsCooldown = 0 // When fpsCooldown <= 0, set displayFPS = fps
-let displayFPS = 0 // fps displayed on screen, updated every second
+// Author: Vincent C. (Vince14Genius)
+// Code compressed for presentation reasons
 
 /* Togglable Variables */
 
 var cameraRange = 1000
-
-var worldHeightBound = 50
-var worldWidthBound = 200
-var worldDepthBound = 200
-
-/* Variables for keyboard & mouse control */
-
-var oldMouseX = null
-
-var leftKeyDown = false
-var rightKeyDown = false
-var upKeyDown = false
-var downKeyDown = false
-
-var wKeyDown = false
-var aKeyDown = false
-var sKeyDown = false
-var dKeyDown = false
-
-var shiftKeyDown = false
-var spaceKeyDown = false
-
-var paused = false
+var worldHeightBound = 50, worldWidthBound = 200, worldDepthBound = 200
 
 /* Essential variables */
 
@@ -40,23 +15,13 @@ var activeCamera = null
 
 class Vector {
     /// Create a vector object using regular 3D coordinates
-    constructor(x, y, z) {
-        this.x = x
-        this.y = y
-        this.z = z
-    }
+    constructor(x, y, z) { this.x = x; this.y = y; this.z = z }
 
     /// Translate the vector by the given change in 3D coordinate position
-    translateBy(deltaX, deltaY, deltaZ) {
-        this.x += deltaX
-        this.y += deltaY
-        this.z += deltaZ
-    }
+    translateBy(deltaX, deltaY, deltaZ) { this.x += deltaX; this.y += deltaY; this.z += deltaZ }
 
     /// GET the magnitude (modulus) of the vector
-    magnitude() {
-        return Math.sqrt(this.x * this.x + this.y * this.y + this.z * this.z)
-    }
+    magnitude() { return Math.sqrt(this.x * this.x + this.y * this.y + this.z * this.z) }
 }
 
 /// GET the scalar product of two vectors
@@ -73,10 +38,7 @@ function angleBetweenVectors(vectorA, vectorB) {
 
 class Point extends Vector {
     /// Create a vector object representing a single point in the 3D world
-    constructor(x, y, z) {
-        super(x, y, z)
-        this.representationalRadius = 1
-    }
+    constructor(x, y, z) { super(x, y, z) }
 
     /// GET the distance from this point to the camera
     distanceToCamera(camera) {
@@ -89,12 +51,8 @@ class Point extends Vector {
         var pointRelativeToCamera = new Vector(this.x - camera.x, this.y - camera.y, this.z - camera.z)
         var cameraDirection = new Vector(Math.cos(camera.yRotation), 0, Math.sin(camera.yRotation))
         var angle = angleBetweenVectors(pointRelativeToCamera, cameraDirection)
-        if (angle > camera.visionAngle) {
-            return false
-        }
-        if (this.distanceToCamera(camera) > cameraRange) {
-            return false
-        }
+        if (angle > camera.visionAngle) { return false }
+        if (this.distanceToCamera(camera) > cameraRange) { return false }
         return true
     }
 
@@ -150,110 +108,36 @@ class Point extends Vector {
 
         return [xOnScreen, yOnScreen]
     }
-
-    /// Draw the point onto the 2D screen as a sphere in 3D space
-    renderInCamera(camera) {
-        if (!this.shouldRenderInCamera(camera)) {
-            return
-        }
-
-        var positionOnScreen = this.renderPositionInCamera(camera)
-
-        // ratio of sphere size to screen size
-        var screenDiagonalRadius = Math.sqrt(Math.pow(windowWidth / 2, 2) + Math.pow(windowHeight / 2, 2))
-        var occupationRatio = this.representationalRadius / (this.distanceToCamera(camera) * sin(camera.visionAngle))
-
-        // actual size
-        var actualSize = screenDiagonalRadius * occupationRatio
-
-        noStroke()
-        fill(255)
-        ellipse(positionOnScreen[0], positionOnScreen[1], actualSize, actualSize)
-    }
-}
-
-/// Swap two items in an array, with the items given by the provided indices
-function swapInArray(array, indexA, indexB) {
-    // JavaScript Arrays are Objects, not Structs
-    var tempItem = array[indexA]
-    array[indexA] = array[indexB]
-    array[indexB] = tempItem
-}
-
-/// Sort all items in an array by their distances to the given camera, using the quicksort algorithm
-function quicksortDistancesToCamera(distances, camera, startIndex, endIndex) {
-    if (endIndex - startIndex <= 0) {
-        return
-    }
-
-    if (endIndex - startIndex + 1 == 2) {
-        if (distances[0].distanceToCamera(camera) < distances[1].distanceToCamera(camera)) {
-            swapInArray(distances, 0, 1)
-        }
-        return
-    }
-
-    var pivot = distances[startIndex + Math.floor((endIndex - startIndex) / 2)].distanceToCamera(camera)
-
-    var i = startIndex
-    var j = endIndex
-    while (i < j) {
-        if (distances[i].distanceToCamera(camera) < pivot) {
-            swapInArray(distances, i, j)
-        } else {
-            i += 1
-        }
-        if (distances[j].distanceToCamera(camera) > pivot) {
-            swapInArray(distances, i, j)
-        } else {
-            j -= 1
-        }
-    }
-
-    quicksortDistancesToCamera(distances, camera, startIndex, i)
-    quicksortDistancesToCamera(distances, camera, i + 1, endIndex)
 }
 
 class Camera extends Vector {
     /// Create a vector object representing a camera, with  vision range and starting y-rotation provided
     constructor(x, y, z, visionAngle, yRotation) {
-        super(x, y, z)
-        this.visionAngle = visionAngle
-        this.yRotation = yRotation
+        super(x, y, z); this.visionAngle = visionAngle; this.yRotation = yRotation
     }
 
     /// Render all items in a 3D world onto the 2D screen through this camera
     renderItemsInWorld(world) {
-        quicksortDistancesToCamera(world, this, 0, world.length - 1)
-        for (var thisItem of world) {
-            thisItem.renderInCamera(this)
-        }
+        var thisCamera = this
+        world.sort(function(a, b) { return a.distanceToCamera(thisCamera) < b.distanceToCamera(thisCamera)})
+        for (var thisItem of world) { thisItem.renderInCamera(this) }
     }
 
     /// Rotate the camera's y-rotation by a certain angle
-    rotateYBy(rotation) {
-        this.yRotation += rotation
-        this.yRotation %= Math.PI
-    }
+    rotateYBy(rotation) { this.yRotation += rotation; this.yRotation %= Math.PI }
 }
 
 class Color {
     /// Create a color object using RGBA values
     constructor(red, green, blue, alpha) {
-        this.red = red
-        this.green = green
-        this.blue = blue
-        this.alpha = alpha
+        this.red = red; this.green = green; this.blue = blue; this.alpha = alpha
     }
 }
 
 class Triangle {
     /// Create a triangle object using three points and one color
     constructor(pointA, pointB, pointC, color) {
-        this.pointA = pointA
-        this.pointB = pointB
-        this.pointC = pointC
-        this.color = color
+        this.pointA = pointA; this.pointB = pointB; this.pointC = pointC; this.color = color
     }
 
     /// Draw the triangle onto the 2D screen using the positions of the translated points
@@ -295,12 +179,7 @@ class Triangle {
 function addTrianglesFromPointGroup(points, triangleIndices) {
     for (var triangleIndexGroup of triangleIndices) {
         var color = new Color(Math.random() * 100 + 155, Math.random() * 100 + 155, Math.random() * 100 + 155, 255)
-        objectsInWorld.push(new Triangle(
-            points[triangleIndexGroup[0]],
-            points[triangleIndexGroup[1]],
-            points[triangleIndexGroup[2]],
-            color
-        ))
+        objectsInWorld.push(new Triangle(points[triangleIndexGroup[0]], points[triangleIndexGroup[1]], points[triangleIndexGroup[2]], color))
     }
 }
 
@@ -321,23 +200,17 @@ function createCube(x, y, z, sideLength) {
 
     var triangleIndices = [
         // front
-        [4, 5, 6],
-        [7, 5, 6],
+        [4, 5, 6], [7, 5, 6],
         // back
-        [0, 1, 2],
-        [3, 1, 2],
+        [0, 1, 2], [3, 1, 2],
         // top
-        [2, 3, 6],
-        [7, 3, 6],
+        [2, 3, 6], [7, 3, 6],
         // bottom
-        [0, 1, 4],
-        [5, 1, 4],
+        [0, 1, 4], [5, 1, 4],
         // left
-        [0, 2, 4],
-        [6, 2, 4],
+        [0, 2, 4], [6, 2, 4],
         // right
-        [1, 3, 5],
-        [7, 3, 5],
+        [1, 3, 5], [7, 3, 5],
     ]
 
     addTrianglesFromPointGroup(points, triangleIndices)
@@ -387,38 +260,68 @@ function createTowerShape(x, y, z, sideLength) {
     createPyramid(x, y - 2 * sideLength, z, sideLength)
 }
 
+/* Variables for keyboard & mouse control */
+
+var oldMouseX = null
+
+var leftKeyDown = false, rightKeyDown = false, upKeyDown = false, downKeyDown = false
+var wKeyDown = false, aKeyDown = false, sKeyDown = false, dKeyDown = false
+var shiftKeyDown = false, spaceKeyDown = false
+
+var paused = false
+
+/* FPS Engine*/
+
+var fpsEngine = {
+    oldTime: 0, fpsCooldown: 0, displayFPS: 0, realFPS: 0, 
+    updateFPS: function() {
+        // Update realFPS
+        var currentTime = millis(); var fpsDelta = currentTime - this.oldTime
+        this.realFPS = frameRate() // function provided by p5.js API
+        
+        // Update displayFPS
+        this.fpsCooldown -= fpsDelta
+        if (this.fpsCooldown <= 0) { this.displayFPS = this.realFPS; this.fpsCooldown = 1000 }
+        this.oldTime = currentTime
+    },
+    showFPS: function(r, g, b) {
+        fill(r, g, b); textAlign(RIGHT, TOP); textSize(24)
+        text(`${Math.floor(this.displayFPS)} fps`, width - 16, 16)
+    }
+}
+
 /* Event handlers */
 
 function setup() {
-    /* setup() runs once at the initialization of the scene. Put your setup code here. */
+    /* setup() runs once at the initialization of the scene. */
 
     activeCamera = new Camera(0, 0, 0, Math.PI / 4, 0)
     createCanvas(windowWidth, windowHeight)
 
-    // Add 50 points and small triangles into the 3D world
-    for (var i = 0; i < 100; i++) {
-        objectsInWorld.push(new Point(Math.random() * 200 - 100, Math.random() * 50 - 25, Math.random() * 200 - 100))
+    function normalizedRandom(range) { return (Math.random() - 0.5) * range }
 
-        var pointA = new Point(Math.random() * 200 - 100, Math.random() * 50 - 25, Math.random() * 200 - 100)
-        var pointB = new Point(pointA.x + Math.random() * 8 - 4, pointA.y + Math.random() * 8 - 4, pointA.z + Math.random() * 8 - 4)
-        var pointC = new Point(pointA.x + Math.random() * 8 - 4, pointA.y + Math.random() * 8 - 4, pointA.z + Math.random() * 8 - 4)
+    // Add 100 small triangles into the 3D world
+    for (var i = 0; i < 100; i++) {
+        var pointA = new Point(normalizedRandom(worldWidthBound), normalizedRandom(worldHeightBound), normalizedRandom(worldDepthBound))
+        var pointB = new Point(pointA.x + normalizedRandom(8), pointA.y + normalizedRandom(8), pointA.z + normalizedRandom(8))
+        var pointC = new Point(pointA.x + normalizedRandom(8), pointA.y + normalizedRandom(8), pointA.z + normalizedRandom(8))
         var color = new Color(Math.random() * 100 + 155, Math.random() * 100 + 155, Math.random() * 100 + 155, Math.random() * 100 + 155)
         objectsInWorld.push(new Triangle(pointA, pointB, pointC, color))
     }
 
     // Add 20 cubes into the 3D world
     for (var i = 0; i < 20; i++) {
-        createCube(Math.random() * 200 - 100, Math.random() * 50 - 25, Math.random() * 200 - 100, Math.random() * 4 + 2)
+        createCube(normalizedRandom(worldWidthBound), normalizedRandom(worldHeightBound), normalizedRandom(worldDepthBound), Math.random() * 4 + 2)
     }
 
     // Add 20 pyramids into the 3D world
     for (var i = 0; i < 20; i++) {
-        createPyramid(Math.random() * 200 - 100, Math.random() * 50 - 25, Math.random() * 200 - 100, Math.random() * 4 + 2)
+        createPyramid(normalizedRandom(worldWidthBound), normalizedRandom(worldHeightBound), normalizedRandom(worldDepthBound), Math.random() * 4 + 2)
     }
 
     // Add 20 weird tower shapes into the 3D world
     for (var i = 0; i < 20; i++) {
-        createTowerShape(Math.random() * 200 - 100, Math.random() * 50 - 25, Math.random() * 200 - 100, Math.random() * 4 + 2)
+        createTowerShape(normalizedRandom(worldWidthBound), normalizedRandom(worldHeightBound), normalizedRandom(worldDepthBound), Math.random() * 4 + 2)
     }
 }
 
@@ -429,10 +332,8 @@ function draw() {
 
     if (paused) {
         fill(200, 255, 255)
-        stroke(0)
-        strokeWeight(32)
-        textAlign(CENTER, CENTER)
-        textSize(64)
+        stroke(0); strokeWeight(32)
+        textAlign(CENTER, CENTER); textSize(64)
         text("- P A U S E D -", windowWidth / 2, windowHeight / 2)
         return
     }
@@ -443,151 +344,75 @@ function draw() {
 
     if (leftKeyDown || aKeyDown) {
         var direction = activeCamera.yRotation - Math.PI / 2
-        activeCamera.x += Math.cos(direction)
-        activeCamera.z += Math.sin(direction)
+        activeCamera.x += Math.cos(direction); activeCamera.z += Math.sin(direction)
     }
 
     if (rightKeyDown || dKeyDown) {
         var direction = activeCamera.yRotation + Math.PI / 2
-        activeCamera.x += Math.cos(direction)
-        activeCamera.z += Math.sin(direction)
+        activeCamera.x += Math.cos(direction); activeCamera.z += Math.sin(direction)
     }
 
     if (upKeyDown || wKeyDown) {
         var direction = activeCamera.yRotation
-        activeCamera.x += Math.cos(direction)
-        activeCamera.z += Math.sin(direction)
+        activeCamera.x += Math.cos(direction); activeCamera.z += Math.sin(direction)
     }
 
     if (downKeyDown || sKeyDown) {
         var direction = activeCamera.yRotation + Math.PI
-        activeCamera.x += Math.cos(direction)
-        activeCamera.z += Math.sin(direction)
+        activeCamera.x += Math.cos(direction); activeCamera.z += Math.sin(direction)
     }
 
-    if (spaceKeyDown) {
-        activeCamera.y += 1
-    }
-
-    if (shiftKeyDown) {
-        activeCamera.y -= 1
-    }
+    if (spaceKeyDown) { activeCamera.y += 1 }
+    if (shiftKeyDown) { activeCamera.y -= 1 }
 
     /* Check for world boundaries */
 
-    activeCamera.x = (activeCamera.x < -worldWidthBound / 2) ? -(worldWidthBound / 2) : activeCamera.x
+    activeCamera.x = (activeCamera.x < -worldWidthBound / 2) ? (-worldWidthBound / 2) : activeCamera.x
     activeCamera.x = (activeCamera.x > worldWidthBound / 2) ? (worldWidthBound / 2) : activeCamera.x
 
-    activeCamera.y = (activeCamera.y < -worldHeightBound / 2) ? -(worldHeightBound / 2) : activeCamera.y
+    activeCamera.y = (activeCamera.y < -worldHeightBound / 2) ? (-worldHeightBound / 2) : activeCamera.y
     activeCamera.y = (activeCamera.y > worldHeightBound / 2) ? (worldHeightBound / 2) : activeCamera.y
 
-    activeCamera.z = (activeCamera.z < -worldDepthBound / 2) ? -(worldDepthBound / 2) : activeCamera.z
+    activeCamera.z = (activeCamera.z < -worldDepthBound / 2) ? (-worldDepthBound / 2) : activeCamera.z
     activeCamera.z = (activeCamera.z > worldDepthBound / 2) ? (worldDepthBound / 2) : activeCamera.z
 
-    /* Render */
-
+    // Render
     background(0)
+    if (activeCamera !== null) { activeCamera.renderItemsInWorld(objectsInWorld) }
 
-    if (activeCamera !== null) {
-        activeCamera.renderItemsInWorld(objectsInWorld)
-    }
-
-    /* Display FPS */
-
-    var currentTime = millis()
-    frameTimes.push(currentTime)
-    while (frameTimes.length > 0 && frameTimes[0] < currentTime - 1000) {
-        frameTimes.splice(0, 1)
-    }
-
-    fpsCooldown -= currentTime - oldTime
-    if (fpsCooldown <= 0) {
-        displayFPS = frameTimes.length
-        fpsCooldown = 1000
-    }
-
-    fill(100)
-    textAlign(RIGHT, TOP) // Text alignment of the fps label
-    textSize(24)
-    text(`${Math.floor(displayFPS)} fps`, width - 16, 16) // Position of the fps label
-
-    oldTime = currentTime
+    // Update and show FPS
+    fpsEngine.updateFPS(); fpsEngine.showFPS(200, 200, 200)
 }
 
-function windowResized() {
-    resizeCanvas(windowWidth, windowHeight)
-}
-
-function mousePressed() {
-    paused = !paused
-}
+function windowResized() { resizeCanvas(windowWidth, windowHeight) }
+function mousePressed() { paused = !paused }
 
 function keyPressed() {
     switch (keyCode) {
-        case (LEFT_ARROW):
-            leftKeyDown = true
-            break
-        case (RIGHT_ARROW):
-            rightKeyDown = true
-            break
-        case (UP_ARROW):
-            upKeyDown = true
-            break
-        case (DOWN_ARROW):
-            downKeyDown = true
-            break
-        case (87): // W
-            wKeyDown = true
-            break
-        case (65): // A
-            aKeyDown = true
-            break
-        case (83): // S
-            sKeyDown = true
-            break
-        case (68): // D
-            dKeyDown = true
-            break
-        case (32): // Space
-            spaceKeyDown = true
-            break
-        case (16): // Shift
-            shiftKeyDown = true
-            break
+        case (LEFT_ARROW): leftKeyDown = true; break
+        case (RIGHT_ARROW): rightKeyDown = true; break
+        case (UP_ARROW): upKeyDown = true; break
+        case (DOWN_ARROW): downKeyDown = true; break
+        case (87): /* W */ wKeyDown = true; break
+        case (65): /* A */ aKeyDown = true; break
+        case (83): /* S */ sKeyDown = true; break
+        case (68): /* D */ dKeyDown = true; break
+        case (32): /* Space */ spaceKeyDown = true; break
+        case (16): /* Shift */ shiftKeyDown = true; break
     }
 }
 
 function keyReleased() {
     switch (keyCode) {
-        case (LEFT_ARROW):
-            leftKeyDown = false
-            break
-        case (RIGHT_ARROW):
-            rightKeyDown = false
-            break
-        case (UP_ARROW):
-            upKeyDown = false
-            break
-        case (DOWN_ARROW):
-            downKeyDown = false
-            break
-        case (87): // W
-            wKeyDown = false
-            break
-        case (65): // A
-            aKeyDown = false
-            break
-        case (83): // S
-            sKeyDown = false
-            break
-        case (68): // D
-            dKeyDown = false
-            break
-        case (32): // Space
-            spaceKeyDown = false
-            break
-        case (16): // Shift
-            shiftKeyDown = false
-            break
+        case (LEFT_ARROW): leftKeyDown = false; break
+        case (RIGHT_ARROW): rightKeyDown = false; break
+        case (UP_ARROW): upKeyDown = false; break
+        case (DOWN_ARROW): downKeyDown = false; break
+        case (87): /* W */ wKeyDown = false; break
+        case (65): /* A */ aKeyDown = false; break
+        case (83): /* S */ sKeyDown = false; break
+        case (68): /* D */ dKeyDown = false; break
+        case (32): /* Space */ spaceKeyDown = false; break
+        case (16): /* Shift */ shiftKeyDown = false; break
     }
 }
